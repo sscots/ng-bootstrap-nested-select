8be907da07197760c92153cc9b0d1bb81fba35c8
Scot Schroeder <scotschroeder@gmail.com>
2017-09-18 19:18:28 -0500

    Providing settings

--- src/ng-bootstrap-nested-select.component.ts
+++ src/ng-bootstrap-nested-select.component.ts
@@ -6,8 +6,13 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
     styleUrls: ['./ng-bootstrap-nested-select.component.scss']
 })
 export class NgBootstrapNestedSelectComponent {
-    @Input() items: any = [];
+    @Input() options: any = [];
     @Input() default: any = {};
+    @Input() settings: {
+        field: string,
+        scroll: boolean,
+        top: boolean
+    };
     @Output() selected: EventEmitter<any> = new EventEmitter();
     level: number = 1;
     _selected: string = '';
@@ -15,11 +20,14 @@ export class NgBootstrapNestedSelectComponent {
     constructor() { }
 
     ngOnInit() {
+        if(!this.settings.field) this.settings.field = 'options';
+        if(!this.settings['scroll']) this.settings.scroll = true;
+        if(!this.settings['top']) this.settings.top = false;
         this._selected = this.default;
     }
 
     selectOption(event, option) {
-        if(option.products.length) event.stopPropagation()
+        if(option[this.settings.field].length) event.stopPropagation()
         else {
             this._selected = option;
             this.selected.emit(this._selected);
